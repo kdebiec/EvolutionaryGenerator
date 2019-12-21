e52bb8f93c788ec30f0ed5a10253b4b09d0daa3d
Konrad Dębiec <konradd@tutanota.com>
2019-12-21 21:52:02 +0100

    Initial values are now imported from parameters.json

---
+++ src/agh/cs/main/InitialParameters.java
@@ -0,0 +1,44 @@
+package agh.cs.main;
+
+public class InitialParameters {
+    private int width;
+    private int height;
+    private int startEnergy;
+    private int moveEnergy;
+    private int plantEnergy;
+    private float jungleRatio;
+    private int initialNumberOfAnimals;
+    private int animalMinimumEnergyToProcreate;
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getStartEnergy() {
+        return startEnergy;
+    }
+
+    public int getMoveEnergy() {
+        return moveEnergy;
+    }
+
+    public int getPlantEnergy() {
+        return plantEnergy;
+    }
+
+    public float getJungleRatio() {
+        return jungleRatio;
+    }
+
+    public int getInitialNumberOfAnimals() {
+        return initialNumberOfAnimals;
+    }
+
+    public int getAnimalMinimumEnergyToProcreate() {
+        return animalMinimumEnergyToProcreate;
+    }
+}
--- src/agh/cs/main/World.java
+++ src/agh/cs/main/World.java
@@ -1,19 +1,34 @@
 package agh.cs.main;
 
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class World {
     public static void main(String[] args) {
-        int width = 10;
-        int height = 10;
-        Plant.PLANT_ENERGY = 10;
-        Animal.INITIAL_ENERGY = 50;
-        Animal.MIN_ENERGY_TO_POPULATE = 5;
-        Animal.MOVE_ENERGY_COST = 2;
-        double jungleRatio = 0.1; // From 0 to 1 range
-        IWorldMap map = new Map(width, height, jungleRatio);
-        map.spawnOrigin(4); // Spawns first animals
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader("parameters.json"));
+        } catch (FileNotFoundException e) {
+            System.out.println("No file found! It should be named parameters.json");
+            return;
+        }
+
+        Gson gson = new Gson();
+        InitialParameters initialParameters = gson.fromJson(reader, InitialParameters.class);
+
+        Plant.PLANT_ENERGY = initialParameters.getPlantEnergy();
+        Animal.INITIAL_ENERGY = initialParameters.getStartEnergy();
+        Animal.MIN_ENERGY_TO_POPULATE = initialParameters.getAnimalMinimumEnergyToProcreate();
+        Animal.MOVE_ENERGY_COST = initialParameters.getMoveEnergy();
+        IWorldMap map = new Map(initialParameters.getWidth(),
+                initialParameters.getHeight(),
+                initialParameters.getJungleRatio());
+        map.spawnOrigin(initialParameters.getInitialNumberOfAnimals()); // Spawns first animals
         System.out.print(map.toString());
 
-        int i = 0;
         while (true) {
             map.nextDay();
             System.out.print(map.toString());
